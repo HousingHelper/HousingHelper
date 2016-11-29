d7f8f605166b293e219e7e735a958a198055c8cf
Michael Hanks <mphanks@msn.com>
2016-11-28 17:09:59 -0700

    normalized directives to fixed so only page content will scroll. other basic css styling for future styling

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: 3000,
+    MASSIVE_URI: 'postgres://efgmqawb:18f3dfrxm-Xgnfwi9-u5vjZ-d5zn7mJX@elmer.db.elephantsql.com:5432/efgmqawb',
+    SESSION_SECRET: 'lahsdl;ahsf;lka;ejhfq;oif3lkaeg;oo83h/qwr/lh;ow8eghe/f/',
+};
