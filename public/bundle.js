a1fb28cf692c80419d5f09020dc0b78280ae24aa
Jacob <admin@Mjolnir.local>
2016-11-28 22:09:45 -0700

    working on landing page

--- public/bundle.js
+++ public/bundle.js
@@ -65,7 +65,7 @@ angular.module("housinghelper").controller("adminMainCtrl", ["$scope", function 
 }]);
 // INITILIZE CONTROLLER
 // ============================================================
-angular.module("housinghelper").controller("apartmentsCtrl", ["$scope", function ($scope) {
+angular.module("housinghelper").controller("createApartmentCtrl", ["$scope", function ($scope) {
   // VARIABLES
   // ============================================================
   $scope.test = 'testerino';
@@ -74,7 +74,7 @@ angular.module("housinghelper").controller("apartmentsCtrl", ["$scope", function
 }]);
 // INITILIZE CONTROLLER
 // ============================================================
-angular.module("housinghelper").controller("createApartmentCtrl", ["$scope", function ($scope) {
+angular.module("housinghelper").controller("apartmentsCtrl", ["$scope", function ($scope) {
   // VARIABLES
   // ============================================================
   $scope.test = 'testerino';
@@ -98,6 +98,72 @@ angular.module("housinghelper").controller("homeCtrl", ["$scope", function ($sco
   $scope.test = "testerino";
   // FUNCTIONS
   // ============================================================
+  $(function () {
+    // Select link by id and add click event
+    $('#link1').click(function () {
+
+      // Animate Scroll to #bottom
+      $('html,body').animate({
+        scrollTop: $("#part-one").offset().top }, // Tell it to scroll to the top #bottom
+      2000 // How long scroll will take in milliseconds
+      );
+
+      // Prevent default behavior of link
+      return false;
+    });
+  });
+  $(function () {
+    // Select link by id and add click event
+    $('#link2').click(function () {
+
+      // Animate Scroll to #bottom
+      $('html,body').animate({
+        scrollTop: $("#part-two").offset().top }, // Tell it to scroll to the top #bottom
+      2000 // How long scroll will take in milliseconds
+      );
+
+      // Prevent default behavior of link
+      return false;
+    });
+  });
+  $(function () {
+    // Select link by id and add click event
+    $('#link3').click(function () {
+
+      // Animate Scroll to #bottom
+      $('html,body').animate({
+        scrollTop: $("#part-three").offset().top }, // Tell it to scroll to the top #bottom
+      2000 // How long scroll will take in milliseconds
+      );
+
+      // Prevent default behavior of link
+      return false;
+    });
+  });
+  // this is where im going to use window scroll to animate the nav bar*****************
+  $(window).scroll(function () {
+    var scrollLo = $(this).scrollTop();
+    // turn the background to clear and fix the nav bar
+    if (scrollLo > 400) {
+      $("nav").css({
+        "position": "fixed",
+        "top": "0px",
+        "z-index": "10"
+      });
+      $(".aboutCon").css({
+        "margin-top": "46px"
+      });
+    } else {
+      $("nav").css({
+        "position": "static"
+      });
+      $(".aboutCon").css({
+        "margin-top": "0px"
+      });
+    }
+
+    // console.log(scrollLo);
+  });
 }]);
 // INITILIZE CONTROLLER
 // ============================================================
