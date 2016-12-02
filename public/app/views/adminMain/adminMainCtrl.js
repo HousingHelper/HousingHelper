3ff6e423b88af3df9af47ef0c306e61f1206c303
Michael Hanks <mphanks@msn.com>
2016-12-02 10:43:21 -0700

    more login stuff

--- public/app/views/adminMain/adminMainCtrl.js
+++ public/app/views/adminMain/adminMainCtrl.js
@@ -1,11 +1,9 @@
 // INITILIZE CONTROLLER
 // ============================================================
-angular.module("housinghelper").controller("adminMainCtrl", function($scope) { //apartments resolve?
+angular.module("housinghelper").controller("adminMainCtrl", function($scope, admin) { //apartments resolve?
   // VARIABLES
   // ============================================================
-  // $scope.testwers = 'testerino';
-  // console.log('Ctrl: ', apartments);
-  // $scope.apts = apartments;
+  $scope.admin = admin;
   // FUNCTIONS
   // ============================================================
 });
