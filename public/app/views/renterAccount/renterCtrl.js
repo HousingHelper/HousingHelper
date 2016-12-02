9d5d2bef279384e52c5d4a585df2bab6771a83eb
hilljacob <hillboy14@gmail.com>
2016-12-01 19:10:34 -0700

    finished landing page and more

--- public/app/views/renterAccount/renterCtrl.js
+++ public/app/views/renterAccount/renterCtrl.js
@@ -3,6 +3,8 @@
 angular.module("housinghelper").controller("renterCtrl", function($scope, acc, apt, servReq) {
   // VARIABLES
   // ============================================================
+    $scope.hide = true
+    $scope.show = true
     $scope.acc = acc;
     $scope.apt = apt;
     $scope.servReq = servReq;
