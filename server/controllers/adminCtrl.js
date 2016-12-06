bb676852c540ad7080ae33defc0d427ca06b46e4
hilljacob <hillboy14@gmail.com>
2016-12-06 11:06:48 -0700

    fixed issues for adminHOme

--- server/controllers/adminCtrl.js
+++ server/controllers/adminCtrl.js
@@ -157,38 +157,38 @@ module.exports = {
     db.get_all_admin_apts([admin.id], function (err, apts) {
       res.status(200).send(apts)
     })
-  }
-    // getAllApartmentsWithRenters: function (req, res, next) {
-    //   // var admin = req.user[0]
-    //   db.please_work([86], function (err, renters) {
-    //     if (err){
-    //     res.send("error: ", err)
-    //   }
-    //     res.status(200).json(renters)
-    //   })
-    // },
+  },
+    getAllApartmentsWithRenters: function (req, res, next) {
+      // var admin = req.user[0]
+      db.please_work([86], function (err, renters) {
+        if (err){
+        res.send("error: ", err)
+      }
+        res.status(200).json(renters)
+      })
+    },
 
 
 
     getAllFaqs: function(req, res) {
         var user = req.user[0]
-        console.log('this is your user', user);
+        // console.log('this is your user', user);
         db.get_all_faqs([user.id], function(err, faqs) {
             if (err) {
                 res.send("error: ", err)
             }
-            res.status(200).json(faqs)
+            res.status(200).send(faqs)
         })
     },
 
-    getAptsByAdminId: function(req, res) {
-        db.apartments.where("admin_id=$1", [req.params.adminId], function(err, apartments) {
-            if (err) {
-                res.send("error: ", err)
-            }
-            res.status(200).send(apartments)
-        })
-    },
+    // getAptsByAdminId: function(req, res) {
+    //     db.apartments.where("admin_id=$1", [req.params.adminId], function(err, apartments) {
+    //         if (err) {
+    //             res.send("error: ", err)
+    //         }
+    //         res.status(200).send(apartments)
+    //     })
+    // },
 
     getRentersByAptId: function(req, res) {
         var apartment = req.params
