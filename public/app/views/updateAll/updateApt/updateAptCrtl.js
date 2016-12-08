927d1da7d4b4914223a8a7dd066793c4fefcae29
DanielTobar <danieltobar4611@gmail.com>
2016-12-08 14:38:15 -0700

    inputs

--- public/app/views/updateAll/updateApt/updateAptCrtl.js
+++ public/app/views/updateAll/updateApt/updateAptCrtl.js
@@ -3,7 +3,7 @@
 angular.module("housinghelper").controller("updateApt", function($scope) {
   // VARIABLES
   // ============================================================
-
+  
 
   // FUNCTIONS
   // ============================================================
