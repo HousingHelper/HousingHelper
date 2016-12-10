430aab6de22ff50326d9d4e5fbff97a33cd63960
hilljacob <hillboy14@gmail.com>
2016-12-09 19:34:54 -0700

    i've been fixing little things

--- public/app/views/landingPage/homeCtrl.js
+++ public/app/views/landingPage/homeCtrl.js
@@ -62,20 +62,20 @@ angular.module("housinghelper").controller("homeCtrl", function($scope, loginSer
           return false;
         });
       });
-      $(function() {
-            // Select link by id and add click event
-            $('#link3').click(function() {
+    $(function() {
+          // Select link by id and add click event
+          $('#link3').click(function() {
 
-              // Animate Scroll to #bottom
-              $('html,body').animate({
-                scrollTop: $("#part-three").offset().top }, // Tell it to scroll to the top #bottom
-                2000 // How long scroll will take in milliseconds
-              );
+            // Animate Scroll to #bottom
+            $('html,body').animate({
+              scrollTop: $("#part-three").offset().top }, // Tell it to scroll to the top #bottom
+              2000 // How long scroll will take in milliseconds
+            );
 
-              // Prevent default behavior of link
-              return false;
-            });
+            // Prevent default behavior of link
+            return false;
           });
+        });
       // this is where im going to use window scroll to animate the nav bar*****************
       $(window).scroll(function(){
             var scrollLo = $(this).scrollTop();
